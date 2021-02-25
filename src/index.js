982b24fca2bd2e879fd1bb2f7e6012a55ba7c196
Brenda Ferrufino <ferrufinob@gmail.com>
2021-02-25 18:40:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
+"use strict";
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("create-task-form");
-  const taskDesc = document.getElementById("new-task-description");
-  const taskUL = document.getElementById("tasks");
-  const newItem = document.createElement("li");
-
-  form.addEventListener("submit", addListItem);
-  function addListItem(e) {
-    e.preventDefault();
-    console.log("button clicked");
-    taskUL.innerHTML += `<li id="list">${taskDesc.value} <button id="delete-btn">Delete</button></li>`;
-    console.log(taskDesc.value);
-    form.reset();
-  }
+  form.addEventListener("submit", manageListItems);
 });
+
+const form = document.getElementById("create-task-form");
+const taskDesc = document.getElementById("new-task-description");
+
+function manageListItems(e) {
+  e.preventDefault();
+  console.log("button clicked");
+
+  // create list item
+  const newItem = document.createElement("li");
+  console.log(taskDesc.value);
+  newItem.textContent = taskDesc.value;
+  const taskUL = document.getElementById("tasks");
+  taskUL.appendChild(newItem);
+  form.reset();
+
+  // create delete button
+  const deleteButton = document.createElement("button");
+  newItem.appendChild(deleteButton);
+  deleteButton.textContent = "Delete";
+  //delete an individual list item
+  deleteButton.addEventListener("click", () => {
+    taskUL.removeChild(newItem);
+  });
+}
