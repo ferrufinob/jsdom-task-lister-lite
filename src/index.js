d9b7e81edf9e47881d0850ec08f4281afcdc1d6a
Brenda Ferrufino <ferrufinob@gmail.com>
2021-02-25 16:23:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //prevent form from submitting(default behavior)
-  //select form
-  const taskForm = document.getElementById("create-task-form");
-  //add event listener
-  taskForm.addEventListener("submit", function (event) {
-    //prevent default action of submit
-    event.preventDefault();
-    //test to see if it works
+  const form = document.getElementById("create-task-form");
+  const taskDesc = document.getElementById("new-task-description");
+  const taskUL = document.getElementById("tasks");
+  const newItem = document.createElement("li");
+
+  form.addEventListener("submit", addListItem);
+  function addListItem(e) {
+    e.preventDefault();
     console.log("button clicked");
-    //select DOM elements needed to make list
-    const taskDesc = document.getElementById("new-task-description");
-    const taskList = document.getElementById("tasks");
-    //test to see if user input prints out
-    console.log("Task:", taskDesc.value);
-    //append list of user input
-    taskList.innerHTML += `<li>${taskDesc.value}</li>`;
-    //reset form
-    taskForm.reset();
-    const deleteButton = document.createElement("button");
-    taskList.append(deleteButton);
-    deleteButton.addEventListener("click", function (e) {
-      e.target.parentNode.remove();
-    });
-  });
+    taskUL.innerHTML += `<li id="list">${taskDesc.value} <button id="delete-btn">Delete</button></li>`;
+    console.log(taskDesc.value);
+    form.reset();
+  }
 });
